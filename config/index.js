e43e65e5992d6ff5e72d2e74ec960167d5638b58
ekovegeance <ekovegeance7@gmail.com>
2021-06-25 17:26:54 +0700

    update

--- config/index.js
+++ config/index.js
@@ -44,8 +44,7 @@ module.exports = {
   //-- ARTICLES SECTION SETTINGS -----
   // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
   // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
-  mediumRssFeed:
-    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fekovegeance.medium.com%2Ffeed",
+  mediumRssFeed: "https://ekovegeance.medium.com/feed",
   // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
 
   shownArticles: 3,
